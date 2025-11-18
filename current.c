22cb7f0d1ba4b26c2b6ebe2e81956e6459754c8e
Ines Matos <inesgamer2108@gmail.com>
2025-11-18 17:58:41 +0000

    teste todos kernel_x

--- current.c
+++ current.c
@@ -266,7 +266,7 @@ void get_smooth_comp( int n, float* sa, float* sb) {
 void kernel_x( t_current* const current, const float sa, const float sb ){
 
     float3* restrict const J = current -> J;
-
+    #pragma omp parallel for
     for( int i = 0; i < current -> nx; i++) {
 
         float3 fu = J[i + 1];
@@ -284,10 +284,10 @@ void kernel_x( t_current* const current, const float sa, const float sb ){
 
     // Update x boundaries for periodic boundaries
     if ( current -> bc_type == CURRENT_BC_PERIODIC ) {
-      
+        #pragma omp parallel for
         for(int i = -current->gc[0]; i<0; i++)
             J[ i ] = J[ current->nx + i ];
-      
+        #pragma omp parallel for
         for (int i=0; i<current->gc[1]; i++)
             J[ current->nx + i ] = J[ i ];
     }
