d231a7edd0d2005b768571bdbf81a7bb30439cd5
Ines Matos <inesgamer2108@gmail.com>
2025-11-18 18:35:39 +0000

    revert teste1 and teste2

--- current.c
+++ current.c
@@ -284,16 +284,12 @@ void kernel_x( t_current* const current, const float sa, const float sb ){
 
     // Update x boundaries for periodic boundaries
     if ( current -> bc_type == CURRENT_BC_PERIODIC ) {
-        #pragma omp parallel  // <-- Remova a chave aqui
-        {
-            #pragma omp for
-            for(int i = -current->gc[0]; i<0; i++)
-                J[ i ] = J[ current->nx + i ];
-
-            #pragma omp for
-            for (int i=0; i<current->gc[1]; i++)
-                J[ current->nx + i ] = J[ i ];
-        }
+        #pragma omp parallel for
+        for(int i = -current->gc[0]; i<0; i++)
+            J[ i ] = J[ current->nx + i ];
+        #pragma omp parallel for
+        for (int i=0; i<current->gc[1]; i++)
+            J[ current->nx + i ] = J[ i ];
     }
 
 
